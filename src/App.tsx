0e4b6857299352eb243712254f3f4ecd15f29c8d
Pavel Popkov <goil2011@yandex.ru>
2025-07-29 20:58:06 +0300

    main

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.scss";
+import { ContainerComponent } from "./components/UI/ContainerComponent/ContainerComponent";
+import { Header } from "./components/UI/Header/Header";
+import { Main } from "./components/UI/Main/Main";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main
+        title="Navigating the digital landscape for success"
+        discription="Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation."
+      />
+      <ContainerComponent
+        title="Services"
+        subtitle="At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. 
+      These services include:"
+      ></ContainerComponent>
+    </>
+  );
+}
