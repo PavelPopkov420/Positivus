4b465c018cbd1867d13799606cc7d0d858316536
Pavel Popkov <goil2011@yandex.ru>
2025-08-01 19:00:12 +0300

    component Card

---
+++ src/components/UI/Card/Card.tsx
@@ -0,0 +1,38 @@
+import type { FC, ReactNode } from "react";
+import styles from "./Card.module.scss";
+import clsx from "clsx";
+
+interface ICard {
+  title1: string;
+  title2: string;
+  image: ReactNode;
+  children: ReactNode;
+  option: 'Dark' | 'Grey' | 'Green' | 'Dark_Green'
+}
+
+export const Card: FC<ICard> = ({ title1, title2, image, children, option }) => {
+  return (
+    <>
+     
+        <div className={clsx(styles.card,{
+          [styles['card--dark']]: option === 'Dark',
+          [styles['card--dark_green']]: option === 'Dark_Green',
+          [styles['card--grey']]: option === 'Grey',
+          [styles['card--green']]: option === 'Green',
+        })}>
+          <div className={styles.card__discrition}>
+            <h3 className={styles.card__title}>
+              <span>{title1}</span>
+              <span>{title2}</span>
+            </h3>
+            <a href="#" className={styles.card__link}>
+              <button className={styles.card__link_btn}>{children}</button>
+              <p className={styles.card__link_text}>Learn more</p>
+            </a>
+          </div>
+          <div className={styles.card__image}>{image}</div>
+        </div>
+   
+    </>
+  );
+};
