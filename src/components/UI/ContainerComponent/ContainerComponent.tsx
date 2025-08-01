c02eb2de42c0c29c6603705df8b79f13ed2a4fb7
Pavel Popkov <goil2011@yandex.ru>
2025-08-01 19:00:35 +0300

    fix containerComponent

--- src/components/UI/ContainerComponent/ContainerComponent.tsx
+++ src/components/UI/ContainerComponent/ContainerComponent.tsx
@@ -1,29 +1,28 @@
-import type { ComponentType, FC, ReactNode } from "react";
-import styles from  './ContainerComponent.module.scss'
+import type { FC, ReactNode } from "react";
+import styles from './ContainerComponent.module.scss'
+
 interface IContainerComponent {
-  Component?: ComponentType<{ children: ReactNode }>;
+  children?: ReactNode;
   title: string;
   subtitle: string;
 }
 
 export const ContainerComponent: FC<IContainerComponent> = ({
-  Component,
   title,
-  subtitle
+  subtitle,
+  children
 }) => {
   return (
     <>
-      <div className="container">
+      
         <div className={styles.description}>
           <h2 className={styles.description__title}>{title}</h2>
           <p className={styles.description__subtitle}>{subtitle}</p>
         </div>
-        {Component && (  // Рендерим только если передан
-        <Component>
-          <p>Это переданный контент</p>
-        </Component>
-      )}
-      </div>
+        <div>
+          {children}
+        </div>
+   
     </>
   );
 };
