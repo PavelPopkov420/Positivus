cd06a60dc11d5641d834eba8ce030f0451d59324
Pavel Popkov <goil2011@yandex.ru>
2025-08-01 19:01:01 +0300

    fix main

--- src/components/UI/Main/Main.tsx
+++ src/components/UI/Main/Main.tsx
@@ -22,7 +22,7 @@ export const Main: FC<IMain> = ({ title, discription }) => {
 
   return (
     <>
-      <div className="container">
+      
         <div className={style.main_content}>
           <div className={style.main_content__info}>
             <h2 className={style.main_content__title}>{title}</h2>
@@ -36,41 +36,9 @@ export const Main: FC<IMain> = ({ title, discription }) => {
           </div>
         </div>
         <div className={style.main_content__logotypes}>
-          {/* <ul className={style.main_content__list}>
-            <li  className={style.main_content__list_item}>
-              <a href="">
-                <img src="../../public/AMAZON.png" alt="AMAZON" />
-              </a>
-            </li>
-            <li className={style.main_content__list_item}>
-              <a href="">
-                <img src="../../public/DRIBBLE.png" alt="DRIBBLE" />
-              </a>
-            </li>
-            <li className={style.main_content__list_item}>
-              <a href="">
-                <img src="../../public/HUBSPOT.png" alt="HUBSPOT" />
-              </a>
-            </li>
-            <li className={style.main_content__list_item}>
-              <a href="">
-                <img src="../../public/NOTION.png" alt="NOTION" />
-              </a>
-            </li>
-            <li className={style.main_content__list_item}>
-              <a href="">
-                <img src="../../public/NETFLIX.png" alt="NETFLIX" />
-              </a>
-            </li>
-            <li className={style.main_content__list_item}>
-              <a href="">
-                <img src="../../public/ZOOM.png" alt="ZOOM" />
-              </a>
-            </li>
-          </ul> */}
           <List items={logos} className={style.main_content__list} itemClassName={style.main_content__list_item}/>
         </div>
-      </div>
+
     </>
   );
 };
