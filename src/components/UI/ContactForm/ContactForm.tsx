f6fe0d856e08720d50931a8101b025e08a0f7651
Pavel Popkov <goil2011@yandex.ru>
2025-08-13 17:05:50 +0300

    fix contactform

--- src/components/UI/ContactForm/ContactForm.tsx
+++ src/components/UI/ContactForm/ContactForm.tsx
@@ -1,25 +1,32 @@
-import { useState } from 'react';
-import { useForm, type SubmitHandler } from 'react-hook-form';
-import { Button } from '../Button/Button';
-import { FormSelector } from './FormSelector';
-import { FormImage } from './FormImage';
-import { SayHiForm } from '../forms/SayHiForm';
-import { GetAQuoteForm } from '../forms/GetAQuoteForm';
-import type { IContectForm, FormName } from './FormTypes';
-import styles from './ContactForm.module.scss';
+import { useState } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { Button } from "../Button/Button";
+import { FormSelector } from "./FormSelector";
+import { FormImage } from "./FormImage";
+import { SayHiForm } from "../forms/SayHiForm";
+import { GetAQuoteForm } from "../forms/GetAQuoteForm";
+import type { IContectForm, FormName } from "./FormTypes";
+import styles from "./ContactForm.module.scss";
 
 export const ContactForm = () => {
   const [selectedForm, setSelectedForm] = useState<FormName>("SayHi");
-  const { register, handleSubmit } = useForm<IContectForm>();
+  const { register, handleSubmit, reset } = useForm<IContectForm>();
+  const [isSuccsesModalOpen, setIsSuccsesModalOpen] = useState(false);
 
+  const closeModal: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    setIsSuccsesModalOpen(false);
+  };
   const onSubmit: SubmitHandler<IContectForm> = (data) => {
-    console.log('Form submitted:', data);
+    console.log("Form submitted:", data);
+    reset();
+    setIsSuccsesModalOpen(true);
   };
 
   return (
     <form className={styles.fields} onSubmit={handleSubmit(onSubmit)}>
       <div className={styles.fields__form}>
-        <FormSelector 
+        <FormSelector
           selectedForm={selectedForm}
           onFormChange={setSelectedForm}
         />
@@ -30,9 +37,21 @@ export const ContactForm = () => {
           <GetAQuoteForm register={register} />
         )}
 
-        <Button option="Dark">Send Message</Button>
+        <Button option="Dark">
+          {selectedForm === "SayHi" ? "Send Message" : "Get a Quote"}
+        </Button>
       </div>
-
+      {isSuccsesModalOpen && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modal}>
+            <h3>Заявка отправлена!</h3>
+            <p>Мы свяжемся с вами в ближайшее время.</p>
+            <Button option="Dark" onClick={closeModal}>
+              Закрыть
+            </Button>
+          </div>
+        </div>
+      )}
       <FormImage />
     </form>
   );
