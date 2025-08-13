30c760b529e608b0130f348fce4423fc31ce85a6
Pavel Popkov <goil2011@yandex.ru>
2025-08-13 17:05:33 +0300

    fix formselector

--- src/components/UI/ContactForm/FormSelector.tsx
+++ src/components/UI/ContactForm/FormSelector.tsx
@@ -1,28 +1,31 @@
-import { type FormName } from './FormTypes';
-import styles from './ContactForm.module.scss';
+import { type FormName } from "./FormTypes";
+import styles from "./ContactForm.module.scss";
 
 interface FormSelectorProps {
   selectedForm: FormName;
   onFormChange: (formName: FormName) => void;
 }
 
-export const FormSelector = ({ selectedForm, onFormChange }: FormSelectorProps) => (
-    <div className={styles.fields__checkbox}>
-      <label>
-        <input
-          type="checkbox"  
-          checked={selectedForm === "SayHi"}
-          onChange={() => onFormChange("SayHi")}
-        />{" "}
-        Say Hi
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          checked={selectedForm === "GetAQuote"}
-          onChange={() => onFormChange("GetAQuote")}
-        />{" "}
-        Get a Quote
-      </label>
-    </div>
-  );
+export const FormSelector = ({
+  selectedForm,
+  onFormChange,
+}: FormSelectorProps) => (
+  <div className={styles.fields__checkbox}>
+    <label>
+      <input
+        type="checkbox"
+        checked={selectedForm === "SayHi"}
+        onChange={() => onFormChange("SayHi")}
+      />
+      {"Say Hi"}
+    </label>
+    <label>
+      <input
+        type="checkbox"
+        checked={selectedForm === "GetAQuote"}
+        onChange={() => onFormChange("GetAQuote")}
+      />
+      {"Get a Quote"}
+    </label>
+  </div>
+);
